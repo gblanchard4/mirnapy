d0e56a36eb86206d583c0702a05d58d9ff98f0c5
root <root@whiskey.lsuhsc.edu>
2014-06-20 09:51:35 -0500

    initial commit

---
+++ mirna_pipe.py
@@ -0,0 +1,200 @@
+#! /usr/bin/env python
+
+#~~~~~~~~~~~~~~~~~~~~~~
+# Imports
+#~~~~~~~~~~~~~~~~~~~~~~
+from optparse import OptionParser
+import os
+import commands
+import subprocess
+import sys
+import distutils.spawn
+
+__author__ = "Gene Blanchard"
+__email__ = "me@geneblanchard.com"
+
+'''
+Flow to get to SEQEM fils...
+
+Arguments needed are:
+1) Input ` files 
+2) Adapter sequence 
+3) output directory
+4) SEQEM iterations
+5) Database to align to 
+
+
+#fastx clipper
+fastx_clipper -a TGGAATTCTCGGGTGCCAAGG -n -v -l 16 -i A3.fastq -o clipped_files/A3.fastq_clipped_sequence.txt
+
+#align
+bwa aln -n 2 /home/gene/Code/mirnakey/miRNAkey/DB_mature/mouse/mature_dna_mouse.fa clipped_files/A3.fastq_clipped_sequence.txt > alignment_files/A3.fastq_aligned.sai
+
+#sam_gen_cmd
+bwa samse -n 100000 /home/gene/Code/mirnakey/miRNAkey/DB_mature/mouse/mature_dna_mouse.fa alignment_files/A3.fastq_aligned.sai clipped_files/A3.fastq_clipped_sequence.txt > alignment_files/A3.fastq_aligned.sam
+
+#sam_stats
+/home/gene/Code/mirnakey/miRNAkey/pipeline/sam_stats.pl new-format < alignment_files/A3.fastq_aligned.sam
+A3.fastq - adaptor clipping and alignment to mature_dna_mouse.fa
+
+#SEQEM
+/home/gene/Code/mirnakey/miRNAkey/pipeline/make_multi_input.pl /home/gene/Code/mirnakey/miRNAkey/DB_mature/mouse/mature_dna_mouse.fa clipped_files/A3.fastq_clipped_sequence.txt alignment_files/A3.fastq_aligned.sam SEQEM/A3stq_clipped_sequence 1
+/home/gene/Code/mirnakey/miRNAkey/pipeline/SEQEM_linux/SEQEM SEQEM/A3stq_clipped_sequence_regions.txt SEQEM/A3stq_clipped_sequence_reads.txt SEQEM/A3stq_clipped_sequence_mappings.txt 1000 SEQEM/A3stq_clipped_sequence_seqem_out.txt 
+'''
+
+def main():
+	
+	bin_dir = os.path.dirname(sys.argv[0])+'/'
+
+	#~~~~~~~~~~~~~~~~~~~~~~
+	# Parameters
+	#~~~~~~~~~~~~~~~~~~~~~~
+
+	#Create the argument parser
+	parser = OptionParser(usage="Usage: ")
+
+	# input file
+	# -i --input
+	parser.add_option("-i", "--input", action="store", type="string", dest="input", help="The input fasta, for multiple files seperate with a comma")
+
+	# adapter sequence
+	# -a --adapter
+	parser.add_option("-a", "--adapter", action="store", type="string", dest="adapter", default="TGGAATTCTCGGGTGCCAAGG" , help="The adapter sequence")
+
+	# output file
+	# -o --output
+	parser.add_option("-o", "--output", action="store", type="string", dest="output", help="The root output directory")
+
+	# SEQEM iterations 
+	# -s --seqem
+	parser.add_option("-s", "--seqem", action="store", type="string", dest="seqem", default="1000" , help="Seqem iterations")
+
+	# Alignment Fasta
+	# -d --db
+	#parser.add_option("-d", "--db", action="store", type="string", dest="db", help="The .fa to align to")
+
+	# Grab command line args
+	(options, args) = parser.parse_args()
+
+	# Set argument values
+	input_name = options.input
+	adapter = str.upper(options.adapter)
+	output = options.output
+	seqem = options.seqem
+	#db = options.db
+
+	database =  bin_dir+'DB_mature/mouse/mature_dna_mouse.fa'
+
+	#~~~~~~~~~~~~~~~~~~~~~~
+	# Error checking
+	#~~~~~~~~~~~~~~~~~~~~~~
+	ERROR = False
+
+	if input_name == None:
+		print 'ERROR: You need to enter an input file or files\n'
+		ERROR = True
+	else:	
+		path_list = [os.path.abspath(fastq) for fastq in input_name.split(',')]
+
+	if output == None:
+		print 'ERROR: You need to enter an output directory\n'
+		ERROR = True
+	else:
+		output = os.path.abspath(output)+'/'
+
+	bases = "ACGT"
+	if not any(base in adapter for base in bases):
+		print "Your adapter sequence contains invalid bases"
+	 	ERROR = True
+
+	# Check if the output directory exists
+	if not output == None:
+		if not os.path.exists(output):
+			os.makedirs(output)
+
+	# Quit on error
+	if ERROR == True:
+		print "\n Errors found: Use the -h option for more information"
+		sys.exit()
+
+
+	# Run on each file
+	master_command_list = []
+	# Make output directories
+	dirs = ["alignment_files/","clipped_files/","SEQEM/", "counts/", "RSEM/"]
+	for directory in dirs:
+		master_command_list.append("mkdir -p %s%s" % (output, directory))
+
+	for fastq in path_list:
+		 master_command_list.extend(mirna_command_builder(bin_dir, output, adapter, fastq, seqem) )
+
+	shell_file =  output+'commands.txt'
+	with open(shell_file, 'wb') as shell:
+		for command in master_command_list:
+			try:	
+				shell.write(command+'\n')
+				proc = subprocess.Popen(command, shell=True)
+				proc.wait()
+			except OSError:
+				print 'BROKE'
+	#for command in master_command_list:
+	#	print command
+
+	parse_seqem_counts(output)
+
+
+
+
+
+def mirna_command_builder(bin_dir, output, adapter, fastq, seqem ):
+	command_list = []
+	fastq_name = fastq.split('/')[-1] 
+
+	# fastx clipper
+	fastx_cmd = "fastx_clipper -a %s -n -v -l 16 -i %s -o %sclipped_files/%s_clipped_sequence.txt" % (adapter, fastq, output, fastq_name)
+	command_list.append(fastx_cmd)
+
+	# align
+	align_cmd = "bwa aln -n 2 %sDB_mature/mouse/mature_dna_mouse.fa %sclipped_files/%s_clipped_sequence.txt > %salignment_files/%s_aligned.sai" % (bin_dir, output, fastq_name, output, fastq_name)
+	command_list.append(align_cmd)
+
+	# sam_gen_cmd
+	sam_gen_cmd = "bwa samse -n 100000 %sDB_mature/mouse/mature_dna_mouse.fa %salignment_files/%s_aligned.sai %sclipped_files/%s_clipped_sequence.txt > %salignment_files/%s_aligned.sam" % (bin_dir, output, fastq_name, output, fastq_name, output, fastq_name)
+	command_list.append(sam_gen_cmd)
+
+	# sam_stats
+	#sam_stats_cmd = "/home/gene/Code/mirnakey/miRNAkey/pipeline/sam_stats.pl new-format < alignment_files/A3.fastq_aligned.sam"
+	#command_list.append(sam_stats_cmd)
+	
+	# SEQEM
+	make_multi_cmd = "%smake_multi_input.pl %sDB_mature/mouse/mature_dna_mouse.fa %sclipped_files/%s_clipped_sequence.txt %salignment_files/%s_aligned.sam %sSEQEM/%s_clipped_sequence 1" % (bin_dir, bin_dir, output, fastq_name, output, fastq_name, output, fastq_name )
+	command_list.append(make_multi_cmd)
+
+	seqem_cmd = "%sSEQEM %sSEQEM/%s_clipped_sequence_regions.txt %sSEQEM/%s_clipped_sequence_reads.txt %sSEQEM/%s_clipped_sequence_mappings.txt %s %scounts/%s_clipped_sequence_seqem_out.txt " % (bin_dir, output, fastq_name, output, fastq_name, output, fastq_name, seqem, output, fastq_name)
+	command_list.append(seqem_cmd)
+
+	return command_list
+
+def parse_seqem_counts(output):
+	counts_path = output+"counts/"
+	rsem_path =  output+"RSEM/"
+	counts_files = os.listdir(counts_path)
+	for counts_file in counts_files:
+		if counts_file.endswith("_clipped_sequence_seqem_out.txt"):
+			name = rsem_path+counts_file.strip("_clipped_sequence_seqem_out.txt")+".tsv"
+			with open(counts_path+counts_file, 'r') as counts:
+				with open(name, 'w') as outfile:
+					outfile.write("miRNA_name\tread_counts\tmiRNA_name\tread_counts\tread_counts\n")
+					for line in counts:
+						if not line.startswith('#'):
+							split_line = line.strip('\n').split(' ')
+							mir = split_line[1]
+							count = split_line[5]
+							outfile.write("%s\t%s\t%s\t%s\t%s\n" %(mir, count, mir, count, count) )
+
+if __name__ == '__main__':
+	main()
+
+
+
+
